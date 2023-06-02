ae46e5bf68922019ab05297742444e725075280f
Vadim <vzilyas89@gmail.com>
2023-06-02 16:13:32 +0500

    ready

---
+++ src/main/java/app/config/AppConfig.java
@@ -0,0 +1,13 @@
+package app.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "app")
+@Qualifier
+public class AppConfig {
+
+}
