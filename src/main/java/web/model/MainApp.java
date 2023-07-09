ae6c1ef0778e92edb44ff6ec545071640e4fc8ac
Vadim <vzilyas89@gmail.com>
2023-07-09 23:12:40 +0500

    ...

---
+++ src/main/java/web/model/MainApp.java
@@ -0,0 +1,31 @@
+package web.model;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import web.config.AppConfig;
+import web.service.UserService;
+
+import java.util.List;
+
+public class MainApp {
+    public static void main(String[] args) {
+//        AnnotationConfigApplicationContext context =
+//                new AnnotationConfigApplicationContext(AppConfig.class);
+//
+//        UserService userService = context.getBean(UserService.class);
+//
+//
+//        new User("Nik", "Petrov", 21, "tututu@gmail.com");
+//
+//
+//        List<User> users = userService.getAllUsers();
+//        for (User user : users) {
+//            System.out.println("Name = " + user.getName());
+//            System.out.println("Surname = " + user.getSurname());
+//            System.out.println("Age = " + user.getAge());
+//            System.out.println("Email = " + user.getEmail());
+//            System.out.println();
+//
+//        }
+
+    }
+}
